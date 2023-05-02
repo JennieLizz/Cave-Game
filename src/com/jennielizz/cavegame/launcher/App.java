e802b598d2920c99342323fcecc9507fcbf11295
Jennie <jenniescinocca@gmail.com>
2023-05-02 16:58:28 -0700

    Changing IDEs

--- src/com/jennielizz/cavegame/launcher/App.java
+++ src/com/jennielizz/cavegame/launcher/App.java
@@ -1,36 +1,22 @@
 package com.jennielizz.cavegame.launcher;
 
-import javax.imageio.ImageIO;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
-import java.awt.BorderLayout;
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.image.ImageObserver;
 import java.io.File;
 import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
 
 public class App {
 
     static Image bg;
     static Image shadow;
     static JFrame mainFrame = new JFrame("Cave Game!");
-    static JPanel mainPanel = new JPanel();
+    static JLayeredPane mainPanel = new JLayeredPane();
     static JPanel shadow1 = new JPanel();
-
-    public void paintComponent(Graphics g) {
-        int width = 1280;
-        int height = 720;
-        for (int x = 0; x < width; x += 80) {
-            for (int y = 0; y < height; y += 80) {
-                g.drawImage(bg, x, y, mainPanel);
-            }
-        }
-
-        
-    }
 
     public static void main(String[] args) {
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -42,14 +28,15 @@ public class App {
             e.printStackTrace();
         }
 
-        mainFrame.setLayout(new BorderLayout());
-        mainFrame.add(mainPanel);
-      
-        
+        Graphics g = mainPanel.getGraphics();
+        g.drawImage(bg, 0, 0, mainPanel);
+        g.drawImage(shadow, 0, 0, shadow1);
+
+        mainPanel.setOpaque(false);
         mainFrame.setResizable(true);
         mainFrame.setSize(1280,720);
         mainFrame.setVisible(true);
-
+        
 
         /*
         Manager gameWindow = new Manager();
